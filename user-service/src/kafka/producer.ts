3d68416ef496b37d17b256f6ee5227b365f6e204
Ammar <ammar@ovasave.com>
2025-06-26 09:48:50 +0400

    user controller and router

--- user-service/src/kafka/producer.ts
+++ user-service/src/kafka/producer.ts
@@ -6,7 +6,7 @@ const producer = kafka.producer();
 
 const connectProducer = async () => await producer.connect();
 
-const emitUserCreated = async (user) => {
+export const emitUserCreated = async (user) => {
   await producer.send({
     topic: EVENTS.USER_CREATED,
     messages: [{ value: JSON.stringify(user) }],
