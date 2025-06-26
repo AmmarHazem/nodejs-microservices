d66ba42f076cbd549ca0e0d543a4004130db7e5c
Ammar <ammar@ovasave.com>
2025-06-26 11:42:19 +0400

    add user model

--- user-service/src/controller/user.controller.ts
+++ user-service/src/controller/user.controller.ts
@@ -1,9 +1,10 @@
 import { Request, Response } from "express";
 import { emitUserCreated } from "../kafka/producer";
+import { UserModel } from "../models/UserModel";
 
 export const createUser = async (req: Request, res: Response) => {
   const { name, email } = req.body;
-  const newUser = { id: Date.now(), name, email, orderCount: 0 };
+  const newUser: UserModel = { id: Date.now(), name, email, orderCount: 0 };
   await emitUserCreated(newUser);
   res.status(201).json(newUser);
 };
